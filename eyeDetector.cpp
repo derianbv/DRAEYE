191493898ee83b124c204225d9d5cc731094f860
derianbv <derojasl@unal.edu.co>
2026-04-01 11:51:11 -0500

    first file

---
+++ eyeDetector.cpp
@@ -0,0 +1,18 @@
+//This file needs to open the camera and detect the eye blink
+
+
+#include <iostream>
+
+int main(){
+
+	//use openCV and analize the image. 
+	
+	
+	
+		
+
+	
+	//detect blinking with MediaPipe 
+	
+	return 0;
+}
